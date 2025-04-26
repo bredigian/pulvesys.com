0ecf869662c4427b8d0aeab0985f6509adb7b647
Gianluca Bredice <bredicegian@gmail.com>
2025-04-26 00:34:09 -0300

    feat: add Motion animations to plans section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,8 @@
-import {
-  ENTERPRISE_PLAN_FEATURES,
-  INDIVIDUAL_PLAN_FEATURES,
-} from "../data/plans";
-
-import ArrowLargeIcon from "../components/icons/arrow-large-icon";
-import DotIcon from "../components/icons/dot-icon";
 import FeaturesContainer from "../components/features-container";
 import Footer from "../components/footer";
 import HomeSection from "../sections/home";
 import Navbar from "../components/navbar";
+import PlansSection from "../sections/plans";
 import gianImage from "/about/gian.jpg";
 import matiImage from "/about/mati.jpg";
 
@@ -18,78 +12,7 @@ export default function Home() {
       <Navbar />
       <HomeSection />
       <FeaturesContainer />
-      <section
-        id="plans"
-        className="flex min-h-dvh flex-col items-center justify-center gap-12"
-      >
-        <h4 className="text-6xl font-bold">Planes</h4>
-        <p className="text-center text-2xl font-extralight">
-          Contamos con 2 planes de suscripción.
-          <br />
-          Ambos con{" "}
-          <strong className="font-bold">30 días de prueba gratuíta</strong>, sin
-          necesidad de pagar nada.
-        </p>
-        <div className="flex w-full items-center justify-center gap-24">
-          <div className="flex min-h-[30rem] max-w-[420px] flex-col items-center gap-4 rounded-lg border-2 border-white/10 bg-black/50 px-3 py-6 duration-200 ease-in-out hover:scale-[102%]">
-            <span className="text-2xl font-medium">Plan Individual</span>
-            <h5 className="text-4xl font-bold">
-              {Intl.NumberFormat("es-AR", {
-                style: "currency",
-                currency: "ARS",
-                minimumFractionDigits: 0,
-              })
-                .format(12500)
-                .toString()}
-              /mes
-            </h5>
-            <ul className="flex flex-col gap-1">
-              {INDIVIDUAL_PLAN_FEATURES.map((value) => (
-                <li
-                  key={`${value}_feature`}
-                  className="flex items-start gap-1 opacity-75"
-                >
-                  <DotIcon size={24} />
-                  <p className="max-w-2xs pr-3 text-sm lg:text-base">{value}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="flex min-h-[30rem] max-w-[420px] flex-col items-center gap-4 rounded-lg border-2 border-white/10 bg-black/50 px-3 py-6 duration-200 ease-in-out hover:scale-[102%]">
-            <span className="text-2xl font-medium">Plan Empresa</span>
-            <h5 className="text-4xl font-bold">
-              {Intl.NumberFormat("es-AR", {
-                style: "currency",
-                currency: "ARS",
-                minimumFractionDigits: 0,
-              })
-                .format(30000)
-                .toString()}
-              /mes
-            </h5>
-            <ul className="flex flex-col gap-1">
-              {ENTERPRISE_PLAN_FEATURES.map((value) => (
-                <li
-                  key={`${value}_feature`}
-                  className="flex items-start gap-1 opacity-75"
-                >
-                  <DotIcon size={24} />
-                  <p className="max-w-2xs pr-3 text-sm lg:text-base">{value}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className="flex w-full items-center justify-center gap-24">
-          <p className="text-4xl font-thin">¿Te interesa alguno?</p>
-          <ArrowLargeIcon size={48} to="RIGHT" />
-          <a href="https://myaccount.pulvesys.com" target="_blank">
-            <button className="flex w-2xs cursor-pointer items-center justify-center gap-2 rounded-md bg-gray-300 p-2 font-medium text-black hover:bg-gray-200 active:bg-gray-400">
-              Registrate
-            </button>
-          </a>
-        </div>
-      </section>
+      <PlansSection />
       <section
         id="about"
         className="flex min-h-dvh items-center justify-around"
---
+++ src/sections/plans.tsx
@@ -0,0 +1,154 @@
+import {
+  ENTERPRISE_PLAN_FEATURES,
+  INDIVIDUAL_PLAN_FEATURES,
+} from "../data/plans";
+
+import ArrowLargeIcon from "../components/icons/arrow-large-icon";
+import DotIcon from "../components/icons/dot-icon";
+import { motion } from "motion/react";
+
+export default function PlansSection() {
+  return (
+    <section
+      id="plans"
+      className="flex min-h-dvh flex-col items-center justify-center gap-12"
+    >
+      <motion.h4
+        initial={{ opacity: 0, y: 10, filter: "blur(4px)" }}
+        animate={{ opacity: 50, y: 0, filter: "blur(0px)" }}
+        transition={{
+          duration: 0.5,
+          ease: "easeOut",
+          delay: 0.25,
+        }}
+        className="text-6xl font-bold"
+      >
+        Planes
+      </motion.h4>
+      <motion.p
+        initial={{ opacity: 0, y: 10, filter: "blur(4px)" }}
+        animate={{ opacity: 50, y: 0, filter: "blur(0px)" }}
+        transition={{
+          duration: 0.5,
+          ease: "easeOut",
+          delay: 0.5,
+        }}
+        className="text-center text-2xl font-extralight"
+      >
+        Contamos con 2 planes de suscripción.
+        <br />
+        Ambos con{" "}
+        <strong className="font-bold">30 días de prueba gratuíta</strong>, sin
+        necesidad de pagar nada.
+      </motion.p>
+      <div className="flex w-full items-center justify-center gap-24">
+        <motion.div
+          initial={{ opacity: 0, y: 10, filter: "blur(4px)" }}
+          animate={{ opacity: 50, y: 0, filter: "blur(0px)" }}
+          transition={{
+            duration: 0.5,
+            ease: "easeOut",
+            delay: 0.75,
+          }}
+          className="flex min-h-[30rem] max-w-[420px] flex-col items-center gap-4 rounded-lg border-2 border-white/10 bg-black/50 px-3 py-6"
+        >
+          <span className="text-2xl font-medium">Plan Individual</span>
+          <h5 className="text-4xl font-bold">
+            {Intl.NumberFormat("es-AR", {
+              style: "currency",
+              currency: "ARS",
+              minimumFractionDigits: 0,
+            })
+              .format(12500)
+              .toString()}
+            /mes
+          </h5>
+          <ul className="flex flex-col gap-1">
+            {INDIVIDUAL_PLAN_FEATURES.map((value) => (
+              <li
+                key={`${value}_feature`}
+                className="flex items-start gap-1 opacity-75"
+              >
+                <DotIcon size={24} />
+                <p className="max-w-2xs pr-3 text-sm lg:text-base">{value}</p>
+              </li>
+            ))}
+          </ul>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 10, filter: "blur(4px)" }}
+          animate={{ opacity: 50, y: 0, filter: "blur(0px)" }}
+          transition={{
+            duration: 0.5,
+            ease: "easeOut",
+            delay: 1,
+          }}
+          className="flex min-h-[30rem] max-w-[420px] flex-col items-center gap-4 rounded-lg border-2 border-white/10 bg-black/50 px-3 py-6"
+        >
+          <span className="text-2xl font-medium">Plan Empresa</span>
+          <h5 className="text-4xl font-bold">
+            {Intl.NumberFormat("es-AR", {
+              style: "currency",
+              currency: "ARS",
+              minimumFractionDigits: 0,
+            })
+              .format(30000)
+              .toString()}
+            /mes
+          </h5>
+          <ul className="flex flex-col gap-1">
+            {ENTERPRISE_PLAN_FEATURES.map((value) => (
+              <li
+                key={`${value}_feature`}
+                className="flex items-start gap-1 opacity-75"
+              >
+                <DotIcon size={24} />
+                <p className="max-w-2xs pr-3 text-sm lg:text-base">{value}</p>
+              </li>
+            ))}
+          </ul>
+        </motion.div>
+      </div>
+      <div className="flex w-full items-center justify-center gap-24">
+        <motion.p
+          initial={{ opacity: 0, y: 10, filter: "blur(4px)" }}
+          animate={{ opacity: 50, y: 0, filter: "blur(0px)" }}
+          transition={{
+            duration: 0.5,
+            ease: "easeOut",
+            delay: 1.25,
+          }}
+          className="text-4xl font-thin"
+        >
+          ¿Te interesa alguno?
+        </motion.p>
+        <motion.div
+          initial={{ opacity: 0, y: 10, filter: "blur(4px)" }}
+          animate={{ opacity: 50, y: 0, filter: "blur(0px)" }}
+          transition={{
+            duration: 0.5,
+            ease: "easeOut",
+            delay: 1.5,
+          }}
+        >
+          <ArrowLargeIcon size={48} to="RIGHT" />
+        </motion.div>
+        <motion.a
+          initial={{ opacity: 0, y: 10, filter: "blur(4px)" }}
+          animate={{ opacity: 50, y: 0, filter: "blur(0px)" }}
+          transition={{
+            duration: 0.5,
+            ease: "easeOut",
+            delay: 1.5,
+          }}
+          href="https://myaccount.pulvesys.com"
+          target="_blank"
+        >
+          <button className="flex w-2xs cursor-pointer items-center justify-center gap-2 rounded-md bg-gray-300 p-2 font-medium text-black hover:bg-gray-200 active:bg-gray-400">
+            Registrate
+          </button>
+        </motion.a>
+      </div>
+    </section>
+  );
+}
